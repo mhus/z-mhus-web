baf699a004a05670de1485ec6496c89ac9727466
Mike Hummel <mh@mhus.de>
2016-10-24 02:02:27 +0200

    again jsp, stop working for the moment

--- cherry-processor-jsp/src/main/java/de/mhus/cherry/processor/jsp/JspContext.java
+++ cherry-processor-jsp/src/main/java/de/mhus/cherry/processor/jsp/JspContext.java
@@ -6,6 +6,7 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.HashMap;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +18,7 @@ import org.ops4j.pax.web.jsp.JasperClassLoader;
 import org.ops4j.pax.web.jsp.JspServletWrapper;
 import org.osgi.framework.FrameworkUtil;
 
+import de.mhus.cherry.portal.api.CallContext;
 import de.mhus.cherry.portal.api.VirtualHost;
 import de.mhus.lib.core.directory.ResourceNode;
 
@@ -37,29 +39,29 @@ public class JspContext implements ProcessorContext {
 	private void init() throws ServletException {
 		servletContext = new JspDefaultServletContext(this.host);
 		config = new DefaultServletConfig(servletContext);
-		hostClassLoader = host.getHostClassLoader();
+		hostClassLoader = getClass().getClassLoader();
 		servlet = new JspServletWrapper(null,new MyJasperClassLoader( FrameworkUtil.getBundle(getClass()), hostClassLoader));
 		servlet.init(config);
 	}
 
-	public boolean processRequest(CentralCallContext context, ResourceNode res) {
+	public boolean processRequest(CallContext context, ResourceNode res) {
 
 		//JspRequestWrapper req = new JspRequestWrapper(context, res, host, config);
 		
-		if ( host.getHostClassLoader() != hostClassLoader)
-			try {
-				init();
-			} catch (ServletException e1) {
-				e1.printStackTrace();
-			}
+//		if ( host.getHostClassLoader() != hostClassLoader)
+//			try {
+//				init();
+//			} catch (ServletException e1) {
+//				e1.printStackTrace();
+//			}
 		
-		HttpServletRequest req = context.getRequest();
+		HttpServletRequest req = context.getHttpRequest();
 		ExtendedServletResponse.inject(context);
 		ExtendedServletResponse resp = ExtendedServletResponse.getExtendedResponse(context);
 		try {
 			resp.setStatus(200);
 			resp.setContentType("text/plain");
-			req.setAttribute(Constants.JSP_FILE, context.getTarget());
+			req.setAttribute(Constants.JSP_FILE, );
 			servlet.service(req, resp);
 			resp.flushBuffer();
 		} catch (ServletException | IOException e) {
--- cherry-processor-jsp/src/main/java/de/mhus/cherry/processor/jsp/JspRenderer.java
+++ cherry-processor-jsp/src/main/java/de/mhus/cherry/processor/jsp/JspRenderer.java
@@ -1,31 +1,36 @@
 package de.mhus.cherry.processor.jsp;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+
 import aQute.bnd.annotation.component.Component;
 import de.mhus.cherry.portal.api.CallContext;
 import de.mhus.cherry.portal.api.ResourceRenderer;
 
+// http://stackoverflow.com/questions/1719254/jsp-programmatically-render#1719398
+
 @Component(provide = ResourceRenderer.class, name="cherry_renderer_get_jsp")
 public class JspRenderer implements ResourceRenderer {
 
-	public static final String NAME = "jsp";
+	public static final String NAME = "processor_jsp_context";
 
 	@Override
 	public void doRender(CallContext call) throws Exception {
-		JspContext ctx = (JspContext) call.getVirtualHost().getProcessorContext(NAME);
+		JspContext ctx = (JspContext) call.getSessionContext().getAttribute(NAME);
 		if (ctx == null) {
 			try {
-				ctx = new JspContext(host);
-				host.setProcessorContext(ctx);
+				ctx = new JspContext(call.getVirtualHost());
+				call.getSessionContext().setAttribute(NAME, ctx);
 			} catch (ServletException e) {
 				e.printStackTrace();
 			}
 		}
 		if (ctx == null) {
-			context.getResponse().sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-			return true;
+			call.getVirtualHost().sendError(call,HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			return;
 		}
 		
-		return ctx.processRequest(context,res);
+		return ctx.processRequest(call,call.getResource());
 
 	}
 
