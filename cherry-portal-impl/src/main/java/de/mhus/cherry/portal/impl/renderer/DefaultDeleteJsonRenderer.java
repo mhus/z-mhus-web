c6a78355855ed9c35f938943105309d8fc352974
Mike Hummel <mh@mhus.de>
2016-11-12 02:01:26 +0100

    add default json renderer, because default renderer can be hidden

---
+++ cherry-portal-impl/src/main/java/de/mhus/cherry/portal/impl/renderer/DefaultDeleteJsonRenderer.java
@@ -0,0 +1,9 @@
+package de.mhus.cherry.portal.impl.renderer;
+
+import aQute.bnd.annotation.component.Component;
+import de.mhus.cherry.portal.api.ResourceRenderer;
+
+@Component(provide = ResourceRenderer.class, name="cherry_renderer_delete_json")
+public class DefaultDeleteJsonRenderer extends DefaultDeleteRenderer {
+
+}
