917e425b213ed1b58e8e4f4d87e80334f4d18eb9
Mike Hummel <mikehummel@Nofretete.box>
2016-12-14 23:28:31 +0100

    start using structure control for create

--- cherry-portal-impl/src/main/java/de/mhus/cherry/portal/impl/operation/CreateChildNavigation.java
+++ cherry-portal-impl/src/main/java/de/mhus/cherry/portal/impl/operation/CreateChildNavigation.java
@@ -1,6 +1,7 @@
 package de.mhus.cherry.portal.impl.operation;
 
 import java.util.Collection;
+import java.util.LinkedList;
 
 import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.VerticalLayout;
@@ -12,16 +13,25 @@ import de.mhus.cherry.portal.api.NavNode;
 import de.mhus.cherry.portal.api.WidgetApi;
 import de.mhus.cherry.portal.api.control.EditorFactory;
 import de.mhus.lib.cao.CaoNode;
+import de.mhus.lib.cao.aspect.StructureControl;
+import de.mhus.lib.core.MFile;
 import de.mhus.lib.core.MProperties;
 import de.mhus.lib.core.definition.DefRoot;
+import de.mhus.lib.core.strategy.NotSuccessful;
 import de.mhus.lib.core.strategy.Operation;
 import de.mhus.lib.core.strategy.OperationDescription;
 import de.mhus.lib.core.strategy.OperationResult;
+import de.mhus.lib.core.strategy.Successful;
 import de.mhus.lib.core.strategy.TaskContext;
 import de.mhus.lib.core.util.Pair;
+import de.mhus.lib.form.DataSource;
+import de.mhus.lib.form.Item;
+import de.mhus.lib.form.PropertiesDataSource;
+import de.mhus.lib.form.definition.FmCheckbox;
 import de.mhus.lib.form.definition.FmText;
 import de.mhus.lib.vaadin.operation.AbstractVaadinOperation;
 import de.mhus.lib.vaadin.operation.AbstractVaadinOperationEditor;
+import de.mhus.lib.vaadin.operation.AbstractVaadinOperationForm;
 import de.mhus.osgi.sop.api.Sop;
 
 @Component(properties="tags=control|caonode|create",provide=Operation.class)
@@ -31,40 +41,24 @@ public class CreateChildNavigation extends AbstractVaadinOperation {
 
 	@Override
 	protected AbstractVaadinOperationEditor createEditor() {
-		return new AbstractVaadinOperationEditor() {
+		return new AbstractVaadinOperationForm(this) {
 			
-			private ComboBox pageType;
-
 			@Override
-			protected void initUI() {
+			protected void initDataSource(PropertiesDataSource ds) {
+				MProperties properties = new MProperties();
+				ds.setProperties(properties);
+				
 				CaoNode nav = (CaoNode) editorProperties.get(NODE);
-				// page type
-				pageType = new ComboBox(nls("pageType.caption=Page Type"));
-				pageType.setNullSelectionAllowed(false);
-				pageType.setTextInputAllowed(false);
 				CallContext call = Sop.getApi(CherryApi.class).getCurrentCall();
 				Collection<EditorFactory> list = call.getVirtualHost().getAvailablePageTypes(nav);
-				Object first = null;
+				LinkedList<Item> pageTypeTypes = new LinkedList<>();
 				for (EditorFactory editor : list) {
-					Pair<String, String> pair = new Pair<String,String>(editor.getCaption(), editor.getIdent() );
-					if (first != null) first = pair;
-					pageType.addItem(pair);
+					pageTypeTypes.add(new Item(editor.getIdent(), editor.getCaption() ));
 				}
-				
-				pageType.setWidth("100%");
-				addComponent(pageType);
-				if (first != null) {
-					pageType.select(first);
-					control.canSave(true);
-				}
-			}
 
-			@SuppressWarnings("unchecked")
-			@Override
-			public void fillOperationParameters(MProperties param) {
-				Object v = pageType.getValue();
-				if (v != null)
-					param.setString("pageType", ((Pair<String,String>)v).getValue() );
+				Item[] pageTypeTypesArray = pageTypeTypes.toArray(new Item[pageTypeTypes.size()]);
+				properties.put("pageType." + DataSource.ITEMS, pageTypeTypesArray);
+				
 			}
 			
 		};
@@ -72,7 +66,17 @@ public class CreateChildNavigation extends AbstractVaadinOperation {
 
 	@Override
 	protected OperationResult doExecute2(TaskContext context) throws Exception {
-		return null;
+		CaoNode nav = (CaoNode) context.getParameters().get(NODE);
+		String title = context.getParameters().getString("title");
+		String name = context.getParameters().getString("name", MFile.normalize(title));
+		boolean hidden = context.getParameters().getBoolean("hidden", true);
+		StructureControl control = nav.adaptTo(StructureControl.class);
+		MProperties properties = new MProperties();
+		properties.setString(WidgetApi.RES_TITLE, title);
+		properties.setBoolean(CherryApi.NAV_HIDDEN, hidden);
+		CaoNode res = control.createChildNode(name, properties);
+		if (res == null) return new NotSuccessful(this, "not created", "error=Can't create node", -1);
+		return new Successful(this, "ok", res);
 	}
 
 	@Override
@@ -80,7 +84,8 @@ public class CreateChildNavigation extends AbstractVaadinOperation {
 		return new OperationDescription(this, "Create Child Navigation", new DefRoot(
 					new FmText("pageType", "Page Type", "Type of the new page"),
 					new FmText("title", "Page Title", "Title of the new page"),
-					new FmText("name", "Node Name", "Technical node name shown in path, leave blank for default")
+					new FmText("name", "Node Name", "Technical node name shown in path, leave blank for default"),
+					new FmCheckbox("hidden", "Hidden", "Set node to hidden")
 				));
 	}
 
--- cherry-portal-impl/src/main/java/de/mhus/cherry/portal/impl/operation/CreateSiblingNavigation.java
+++ cherry-portal-impl/src/main/java/de/mhus/cherry/portal/impl/operation/CreateSiblingNavigation.java
@@ -12,20 +12,27 @@ import de.mhus.cherry.portal.api.CherryApi;
 import de.mhus.cherry.portal.api.NavNode;
 import de.mhus.cherry.portal.api.WidgetApi;
 import de.mhus.cherry.portal.api.control.EditorFactory;
+import de.mhus.lib.cao.CaoAspect;
 import de.mhus.lib.cao.CaoNode;
+import de.mhus.lib.cao.aspect.StructureControl;
+import de.mhus.lib.core.MFile;
 import de.mhus.lib.core.MProperties;
 import de.mhus.lib.core.definition.DefRoot;
+import de.mhus.lib.core.strategy.NotSuccessful;
 import de.mhus.lib.core.strategy.Operation;
 import de.mhus.lib.core.strategy.OperationDescription;
 import de.mhus.lib.core.strategy.OperationResult;
+import de.mhus.lib.core.strategy.Successful;
 import de.mhus.lib.core.strategy.TaskContext;
 import de.mhus.lib.core.util.Pair;
 import de.mhus.lib.form.DataSource;
 import de.mhus.lib.form.Item;
 import de.mhus.lib.form.PropertiesDataSource;
+import de.mhus.lib.form.definition.FmCheckbox;
 import de.mhus.lib.form.definition.FmCombobox;
 import de.mhus.lib.form.definition.FmOptions;
 import de.mhus.lib.form.definition.FmText;
+import de.mhus.lib.form.definition.FmValue;
 import de.mhus.lib.vaadin.operation.AbstractVaadinOperation;
 import de.mhus.lib.vaadin.operation.AbstractVaadinOperationEditor;
 import de.mhus.lib.vaadin.operation.AbstractVaadinOperationForm;
@@ -63,7 +70,17 @@ public class CreateSiblingNavigation extends AbstractVaadinOperation {
 
 	@Override
 	protected OperationResult doExecute2(TaskContext context) throws Exception {
-		return null;
+		CaoNode nav = (CaoNode) context.getParameters().get(NODE);
+		String title = context.getParameters().getString("title");
+		String name = context.getParameters().getString("name", MFile.normalize(title));
+		boolean hidden = context.getParameters().getBoolean("hidden", true);
+		StructureControl control = nav.getParent().adaptTo(StructureControl.class);
+		MProperties properties = new MProperties();
+		properties.setString(WidgetApi.RES_TITLE, title);
+		properties.setBoolean(CherryApi.NAV_HIDDEN, hidden);
+		CaoNode res = control.createChildNode(name, properties);
+		if (res == null) return new NotSuccessful(this, "not created", "error=Can't create node", -1);
+		return new Successful(this, "ok", res);
 	}
 
 	@Override
@@ -71,7 +88,8 @@ public class CreateSiblingNavigation extends AbstractVaadinOperation {
 		return new OperationDescription(this, "Create Sibling Navigation", new DefRoot(
 					new FmCombobox("pageType", "Page Type", "Type of the new page"),
 					new FmText("title", "Page Title", "Title of the new page"),
-					new FmText("name", "Node Name", "Technical node name shown in path, leave blank for default")
+					new FmText("name", "Node Name", "Technical node name shown in path, leave blank for default"),
+					new FmCheckbox("hidden", "Hidden", "Set node to hidden")
 				));
 	}
 
