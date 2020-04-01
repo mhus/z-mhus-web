2120735a54c9562330ca9037571d00b30b0a7b21
Mike Hummel <mike.hummel@billingsolutions.de>
2020-04-02 00:39:01 +0200

    Update AaaSessionFilter.java

--- web-util/src/main/java/de/mhus/cherry/web/util/filter/AaaSessionFilter.java
+++ web-util/src/main/java/de/mhus/cherry/web/util/filter/AaaSessionFilter.java
@@ -25,12 +25,11 @@ import de.mhus.cherry.web.api.WebFilter;
 import de.mhus.lib.core.M;
 import de.mhus.lib.core.MLog;
 import de.mhus.lib.core.config.IConfig;
-import de.mhus.lib.core.security.AaaContext;
-import de.mhus.lib.core.security.AccessApi;
+import de.mhus.lib.core.shiro.AccessApi;
 import de.mhus.lib.core.util.Base64;
 import de.mhus.lib.errors.MException;
 
-public class SopSessionFilter extends MLog implements WebFilter {
+public class AaaSessionFilter extends MLog implements WebFilter {
 
     public static final String SESSION_PARAMETER_NAME = "__sop_user_ticket";
     public static final String CONTEXT_PARAMETER_AAA_CONTEXT = "__sop_aaa_context";
@@ -56,8 +55,9 @@ public class SopSessionFilter extends MLog implements WebFilter {
                 if (userTicket == null) return true; // guest?
 
                 Locale locale = req.getLocale();
-                AaaContext userContext = aaa.process(userTicket, locale);
-                call.setAttribute(CONTEXT_PARAMETER_AAA_CONTEXT, userContext);
+
+//XXX                AaaContext userContext = aaa.process(userTicket, locale);
+//                call.setAttribute(CONTEXT_PARAMETER_AAA_CONTEXT, userContext);
             }
         } catch (Throwable t) {
             throw new MException(t);
@@ -67,41 +67,42 @@ public class SopSessionFilter extends MLog implements WebFilter {
 
     @Override
     public void doFilterEnd(UUID instance, InternalCallContext call) throws MException {
-        AaaContext userContext = (AaaContext) call.getAttribute(CONTEXT_PARAMETER_AAA_CONTEXT);
-        if (userContext == null) return;
-
-        AccessApi aaa = M.l(AccessApi.class);
-        aaa.release(userContext);
+//        AaaContext userContext = (AaaContext) call.getAttribute(CONTEXT_PARAMETER_AAA_CONTEXT);
+//        if (userContext == null) return;
+//
+//        AccessApi aaa = M.l(AccessApi.class);
+//        aaa.release(userContext);
     }
 
     public static boolean isLoggedIn(CallContext context) {
-        AaaContext userContext = (AaaContext) context.getAttribute(CONTEXT_PARAMETER_AAA_CONTEXT);
-        return userContext != null;
+//        AaaContext userContext = (AaaContext) context.getAttribute(CONTEXT_PARAMETER_AAA_CONTEXT);
+//        return userContext != null;
+        return false;
     }
 
     public static void login(CallContext context, String user, String pass) throws MException {
 
-        AaaContext userContext = (AaaContext) context.getAttribute(CONTEXT_PARAMETER_AAA_CONTEXT);
-        if (userContext != null)
-            throw new MException("already logged in", userContext.getAccountId());
-
-        AccessApi aaa = M.l(AccessApi.class);
-
-        String userTicket = aaa.createUserTicket(user, pass);
-        context.getSession().setString(SESSION_PARAMETER_NAME, userTicket);
-
-        Locale locale = context.getHttpRequest().getLocale();
-        userContext = aaa.process(userTicket, locale);
-        context.setAttribute(CONTEXT_PARAMETER_AAA_CONTEXT, userContext);
+//        AaaContext userContext = (AaaContext) context.getAttribute(CONTEXT_PARAMETER_AAA_CONTEXT);
+//        if (userContext != null)
+//            throw new MException("already logged in", userContext.getAccountId());
+//
+//        AccessApi aaa = M.l(AccessApi.class);
+//
+//        String userTicket = aaa.createUserTicket(user, pass);
+//        context.getSession().setString(SESSION_PARAMETER_NAME, userTicket);
+//
+//        Locale locale = context.getHttpRequest().getLocale();
+//        userContext = aaa.process(userTicket, locale);
+//        context.setAttribute(CONTEXT_PARAMETER_AAA_CONTEXT, userContext);
     }
 
     public static void logout(CallContext context, String user, String pass) {
 
-        AaaContext userContext = (AaaContext) context.getAttribute(CONTEXT_PARAMETER_AAA_CONTEXT);
-        if (userContext == null) return;
-
-        AccessApi aaa = M.l(AccessApi.class);
-        aaa.release(userContext);
+//        AaaContext userContext = (AaaContext) context.getAttribute(CONTEXT_PARAMETER_AAA_CONTEXT);
+//        if (userContext == null) return;
+//
+//        AccessApi aaa = M.l(AccessApi.class);
+//        aaa.release(userContext);
 
         context.getSession().remove(SESSION_PARAMETER_NAME);
         context.setAttribute(CONTEXT_PARAMETER_AAA_CONTEXT, null);
